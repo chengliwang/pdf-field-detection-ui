7cb6f310fc826976449563d00d624809d09d78ac
cwang3 <cwang3@MISLW5A46A2C8D>
2021-06-20 17:29:45 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Navbar, Nav } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar fixed="top" bg="primary" variant="dark">
+        <Navbar.Brand>
+          <img src="/pdf-icon.png" width="30" height="30" className="d-inline-block align-top" alt="Auto Detect PDF Fields" />
+          {' '}Auto Detect PDF Fields
+        </Navbar.Brand>
+        <Navbar.Collapse className="justify-content-end">
+          <Nav className="me-auto">
+            <Nav.Link href="https://github.com/chengliwang/pdf-field-detection" target="_blank" title="Github">Github</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
     </div>
   );
 }
