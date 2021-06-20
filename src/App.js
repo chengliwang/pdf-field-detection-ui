2cde3a650f3966f454a00c9b10130a9493494291
cwang3 <cwang3@MISLW5A46A2C8D>
2021-06-20 18:46:50 -0400

    feat: add navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Navbar, Nav } from 'react-bootstrap';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <Navbar fixed="top" bg="primary" variant="dark">
-        <Navbar.Brand>
-          <img src="/pdf-icon.png" width="30" height="30" className="d-inline-block align-top" alt="Auto Detect PDF Fields" />
-          {' '}Auto Detect PDF Fields
-        </Navbar.Brand>
-        <Navbar.Collapse className="justify-content-end">
-          <Nav className="me-auto">
-            <Nav.Link href="https://github.com/chengliwang/pdf-field-detection" target="_blank" title="Github">Github</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-
+      <Header />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,28 @@
+import { Navbar, Nav, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { FaGithub } from 'react-icons/fa';
+
+const Header = () => {
+    return(
+      <Navbar fixed="top" bg="primary" variant="dark">
+        <Navbar.Brand>
+          <img src="/pdf-icon.png" width="30" height="30" className="d-inline-block align-top" alt="Auto Detect PDF Fields" />
+          {' '}Auto Detect PDF Fields
+        </Navbar.Brand>
+        <Navbar.Collapse className="justify-content-end">
+          <Nav className="me-auto">
+            <Nav.Link href="https://github.com/chengliwang/pdf-field-detection-ui" target="_blank">             
+              <OverlayTrigger key="bottom" placement="bottom"
+                overlay={
+                  <Tooltip>Github Repo</Tooltip>
+                }
+              >
+                 <FaGithub size="24" />
+              </OverlayTrigger>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+}
+
+export default Header;
