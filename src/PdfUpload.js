c7c70cf25d978149940c7e1a0da37ca981de6972
cwang3 <cwang3@MISLW5A46A2C8D>
2021-06-21 13:34:52 -0400

    fix: update service url

--- src/PdfUpload.js
+++ src/PdfUpload.js
@@ -10,7 +10,7 @@ const PdfUpload = () => {
   let [ label, setLabel ] = useState("");
   let refreshIntervalId;
   let saved = false;
-  let serviceUrl = "https://pdf-field-detection-svc.eastus2.azurecontainer.io";
+  let serviceUrl = "https://pdfmlsvc.azurewebsites.net";
 
   const savePdf = (response) => {
     setProgress(100);
